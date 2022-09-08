a46ed34dc15af331db170008a85e28efea2311dc
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-09-08 16:53:01 +0530

    Add files via upload

---
+++ ArraySorting.java
@@ -0,0 +1,26 @@
+package ArraysAssignment;
+
+import java.util.Arrays;
+
+public class ArraySorting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num[]= {35,56,2,6,7,10};
+System.out.println("The original arrays are ");
+for(int arr:num) {
+	
+	System.out.println(arr);
+}
+Arrays.sort(num);
+System.out.println("The sorted arrays are");
+for(int arr:num)
+{
+System.out.println(arr);
+
+	
+}
+	}
+	}
+
+
