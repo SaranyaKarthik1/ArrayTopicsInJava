4d1620745b380a7421d5949b28a59d1f52c60d2a
SaranyaKarthik1 <111961870+SaranyaKarthik1@users.noreply.github.com>
2022-10-14 18:11:33 +0530

    Add files via upload

---
+++ Stringbufferprogram.java
@@ -0,0 +1,20 @@
+package DailyPrograms;
+
+public class Stringbufferprogram {
+
+	//private static final String Stringbuffer bf = null;
+
+	public static void main(String[] args) {
+		StringBuffer bf=new StringBuffer("sara");
+		String s="saran";
+	
+		  System.out.println(bf.reverse()); System.out.println(bf.append(" karthik"));
+		  System.out.println(bf.hashCode()); System.out.println(bf.capacity());
+		  System.out.println(bf.replace(0, 2, "read"));
+		  System.out.println(bf.charAt(2));
+		 
+		System.out.println(bf.charAt(3));
+
+	}
+
+}
